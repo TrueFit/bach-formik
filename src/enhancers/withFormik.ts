03b85f6ab1a42637bb262bd383677454fdff654e
Josh Gretz <jgretz@truefit.io>
2020-05-05 10:26:18 -0400

    allow caller to specify a lazy loaded eval for the formik configs

--- src/enhancers/withFormik.ts
+++ src/enhancers/withFormik.ts
@@ -1,17 +1,22 @@
-import {EnhancerContext, EnhancerResult} from '@truefit/bach';
+import {EnhancerContext, EnhancerResult, isFunction, PROPS} from '@truefit/bach';
 import {useFormik, FormikConfig} from 'formik';
 
-export default <T>(formikConfig: FormikConfig<T>) => ({
+type FormikConfigParameter<T, P> = FormikConfig<T> | ((props: P) => FormikConfig<T>) | undefined;
+
+export default <T, P>(formikConfig: FormikConfigParameter<T, P>) => ({
   generateNewVariable,
 }: EnhancerContext): EnhancerResult => {
   const formikConfigAlias = generateNewVariable();
+  const resolveFormikConfigAlias = generateNewVariable();
+  const resolveFormikConfig = isFunction(formikConfig) ? formikConfig : () => formikConfig;
 
   return {
     dependencies: {
       useFormik,
-      [formikConfigAlias]: formikConfig,
+      [resolveFormikConfigAlias]: resolveFormikConfig,
     },
     initialize: `
+      const ${formikConfigAlias} = ${resolveFormikConfigAlias}(${PROPS});
       const formik = useFormik(${formikConfigAlias});
     `,
     props: ['formik'],
