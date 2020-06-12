7809c695530933ed54522fa28bb96c5158942363
Josh Gretz <jgretz@truefit.io>
2020-06-12 14:42:19 -0400

    add withFormikContext

---
+++ src/enhancers/withFormikContext.ts
@@ -0,0 +1,17 @@
+import {EnhancerResult} from '@truefit/bach';
+import {useFormikContext} from 'formik';
+
+export default <T>(propertyNames: Array<keyof T> | undefined) => (): EnhancerResult => {
+  const specifiedPropertyNames = propertyNames?.length > 0;
+  const leftSide = specifiedPropertyNames ? `{${propertyNames?.join(', ')}}` : 'formik';
+
+  return {
+    dependencies: {
+      useFormikContext,
+    },
+    initialize: `
+      const ${leftSide} = useFormikContext();
+    `,
+    props: specifiedPropertyNames ? (propertyNames as string[]) : ['formik'],
+  };
+};
