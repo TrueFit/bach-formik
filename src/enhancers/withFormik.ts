cee42b8f412ed8fd7bc2ded54550c2ef076e1f6a
Josh Gretz <jgretz@truefit.io>
2021-06-15 13:36:00 -0400

    update react and depenent libraries

--- src/enhancers/withFormik.ts
+++ src/enhancers/withFormik.ts
@@ -3,32 +3,31 @@ import {useFormik, FormikConfig, FormikProvider} from 'formik';
 
 type FormikConfigParameter<T, P> = FormikConfig<T> | ((props: P) => FormikConfig<T>) | undefined;
 
-export default <T, P>(formikConfig: FormikConfigParameter<T, P>, useContext = true) => ({
-  generateNewVariable,
-}: EnhancerContext): EnhancerResult => {
-  const formikConfigAlias = generateNewVariable();
-  const resolveFormikConfigAlias = generateNewVariable();
-  const resolveFormikConfig = isFunction(formikConfig) ? formikConfig : () => formikConfig;
+export default <T, P>(formikConfig: FormikConfigParameter<T, P>, useContext = true) =>
+  ({generateNewVariable}: EnhancerContext): EnhancerResult => {
+    const formikConfigAlias = generateNewVariable();
+    const resolveFormikConfigAlias = generateNewVariable();
+    const resolveFormikConfig = isFunction(formikConfig) ? formikConfig : () => formikConfig;
 
-  return {
-    dependencies: {
-      FormikProvider,
-      useFormik,
+    return {
+      dependencies: {
+        FormikProvider,
+        useFormik,
 
-      [resolveFormikConfigAlias]: resolveFormikConfig,
-    },
-    initialize: `
+        [resolveFormikConfigAlias]: resolveFormikConfig,
+      },
+      initialize: `
       const ${formikConfigAlias} = ${resolveFormikConfigAlias}(${PROPS});
       const formik = useFormik(${formikConfigAlias});
     `,
-    props: ['formik'],
+      props: ['formik'],
 
-    transformRender: (previousStatement: string): string => {
-      if (!useContext) {
-        return previousStatement;
-      }
+      transformRender: (previousStatement: string): string => {
+        if (!useContext) {
+          return previousStatement;
+        }
 
-      return `${REACT}.createElement(FormikProvider, {value: formik}, ${previousStatement})`;
-    },
+        return `${REACT}.createElement(FormikProvider, {value: formik}, ${previousStatement})`;
+      },
+    };
   };
-};
--- src/enhancers/withFormikContext.ts
+++ src/enhancers/withFormikContext.ts
@@ -1,17 +1,18 @@
 import {EnhancerResult} from '@truefit/bach';
 import {useFormikContext} from 'formik';
 
-export default <T>(propertyNames: Array<keyof T> | undefined) => (): EnhancerResult => {
-  const specifiedPropertyNames = propertyNames?.length > 0;
-  const leftSide = specifiedPropertyNames ? `{${propertyNames?.join(', ')}}` : 'formik';
+export default <T>(propertyNames: Array<keyof T> | undefined) =>
+  (): EnhancerResult => {
+    const specifiedPropertyNames = propertyNames?.length > 0;
+    const leftSide = specifiedPropertyNames ? `{${propertyNames?.join(', ')}}` : 'formik';
 
-  return {
-    dependencies: {
-      useFormikContext,
-    },
-    initialize: `
+    return {
+      dependencies: {
+        useFormikContext,
+      },
+      initialize: `
       const ${leftSide} = useFormikContext();
     `,
-    props: specifiedPropertyNames ? (propertyNames as string[]) : ['formik'],
+      props: specifiedPropertyNames ? (propertyNames as string[]) : ['formik'],
+    };
   };
-};
